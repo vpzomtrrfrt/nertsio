a3ac5c75f70c8d5a3a0ad5133e70d78aca245698
Colin Reeder <colin@vpzom.click>
2026-03-26 17:13:53 -0600

    bots: Fix priority of moves to lake

--- gameserver/src/systems/bots.rs
+++ gameserver/src/systems/bots.rs
@@ -179,47 +179,49 @@ pub(crate) async fn run(global_state: Arc<GlobalState>) {
                                                                 }
                                                             }
 
-                                                            match src {
-                                                                ni_ty::PlayerStackLocation::Tableau(
-                                                                    _,
-                                                                )
-                                                                | ni_ty::PlayerStackLocation::Nerts => {
-                                                                    let src_is_tableau = matches!(src, ni_ty::PlayerStackLocation::Tableau(_));
-                                                                    let count = if src_is_tableau {
-                                                                        stack.len()
-                                                                    } else {
-                                                                        1
-                                                                    };
-                                                                    let back = stack.cards()
-                                                                        [stack.len() - count];
+                                                            if new_plan.is_none() {
+                                                                match src {
+                                                                    ni_ty::PlayerStackLocation::Tableau(
+                                                                        _,
+                                                                    )
+                                                                    | ni_ty::PlayerStackLocation::Nerts => {
+                                                                        let src_is_tableau = matches!(src, ni_ty::PlayerStackLocation::Tableau(_));
+                                                                        let count = if src_is_tableau {
+                                                                            stack.len()
+                                                                        } else {
+                                                                            1
+                                                                        };
+                                                                        let back = stack.cards()
+                                                                            [stack.len() - count];
 
-                                                                    for (i, dest_stack) in hand_player
-                                                                        .tableau_stacks()
-                                                                        .iter()
-                                                                        .enumerate()
-                                                                    {
-                                                                        let dest = ni_ty::StackLocation::Player(idx as u8, ni_ty::PlayerStackLocation::Tableau(i as u8));
-                                                                        if dest_stack.can_add(back) && !dest_stack.is_empty() {
-                                                                            new_plan = Some(ni_ty::HandAction::Move { from: ni_ty::StackLocation::Player(idx as u8, src), to: dest, count: count as u8 }.into());
-                                                                            break;
-                                                                        }
-                                                                    }
-
-                                                                    if new_plan.is_none() && !src_is_tableau {
                                                                         for (i, dest_stack) in hand_player
                                                                             .tableau_stacks()
                                                                             .iter()
                                                                             .enumerate()
                                                                         {
                                                                             let dest = ni_ty::StackLocation::Player(idx as u8, ni_ty::PlayerStackLocation::Tableau(i as u8));
-                                                                            if dest_stack.can_add(back) {
+                                                                            if dest_stack.can_add(back) && !dest_stack.is_empty() {
                                                                                 new_plan = Some(ni_ty::HandAction::Move { from: ni_ty::StackLocation::Player(idx as u8, src), to: dest, count: count as u8 }.into());
                                                                                 break;
                                                                             }
                                                                         }
+
+                                                                        if new_plan.is_none() && !src_is_tableau {
+                                                                            for (i, dest_stack) in hand_player
+                                                                                .tableau_stacks()
+                                                                                .iter()
+                                                                                .enumerate()
+                                                                            {
+                                                                                let dest = ni_ty::StackLocation::Player(idx as u8, ni_ty::PlayerStackLocation::Tableau(i as u8));
+                                                                                if dest_stack.can_add(back) {
+                                                                                    new_plan = Some(ni_ty::HandAction::Move { from: ni_ty::StackLocation::Player(idx as u8, src), to: dest, count: count as u8 }.into());
+                                                                                    break;
+                                                                                }
+                                                                            }
+                                                                        }
                                                                     }
+                                                                    _ => {}
                                                                 }
-                                                                _ => {}
                                                             }
                                                         }
                                                     }
